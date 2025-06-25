262fccf63e015f5514195316339017b042fd7473
Helio Dourado <heliohdd@yahoo.com.br>
2025-06-25 16:23:03 -0300

    API Data Input Schema Definition

--- auth_routes.py
+++ auth_routes.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException
 from models import User
 from dependencies import get_db_session
 from main import bcrypt_context
+from schemas import UserSchema
+from sqlalchemy.orm import Session
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -11,14 +13,14 @@ async def home():
     return {"message": "Auth endpoint"}
 
 @auth_router.post("/create_account")
-async def create_account(username: str, email: str, password: str, session= Depends(get_db_session)):
+async def create_account(user_schema: UserSchema, session: Session = Depends(get_db_session)):
     '''Endpoint to create a new user account'''
-    user = session.query(User).filter(User.email == email).first()
+    user = session.query(User).filter(User.email == user_schema.email).first()
     if user:
         raise HTTPException(status_code=400, detail="Email already registered")
     else:
-        password_hashed = bcrypt_context.hash(password)
-        new_user = User(username, email, password_hashed)  # Assuming User model has email and password fields
+        password_hashed = bcrypt_context.hash(user_schema.password)
+        new_user = User(user_schema.username, user_schema.email, password_hashed, user_schema.active, user_schema.admin)  # Assuming User model has email and password fields
         session.add(new_user)
         session.commit()
-        return {"message": f"User {username} created successfully"}
+        return {"message": f"User {user_schema.username} created successfully"}
---
+++ schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+
+class UserSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+    active: Optional[bool] = True
+    admin: Optional[bool] = False
+
+    class Config:
+        from_attributes = True
