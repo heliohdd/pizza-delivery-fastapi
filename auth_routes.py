107895ce0707b04de5602575f2d05568808a548b
Helio Dourado <heliohdd@yahoo.com.br>
2025-06-26 17:24:51 -0300

    Implementing Access Token Generation for Login Route

--- auth_routes.py
+++ auth_routes.py
@@ -2,10 +2,15 @@ from fastapi import APIRouter, Depends, HTTPException
 from models import User
 from dependencies import get_db_session
 from main import bcrypt_context
-from schemas import UserSchema
+from schemas import LoginSchema, UserSchema
 from sqlalchemy.orm import Session
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
+
+def generate_jwt_token(user_id):
+    '''Function to generate a JWT token for the user'''
+    token = f"ahuyba786dabd86a5vdba865dvad786and{user_id}" # Placeholder for the JWT token generation logic
+    return token
 
 @auth_router.get("/")
 async def home():
@@ -24,3 +29,14 @@ async def create_account(user_schema: UserSchema, session: Session = Depends(get
         session.add(new_user)
         session.commit()
         return {"message": f"User {user_schema.username} created successfully"}
+
+# login -> email e senha -> token JWT (Json Web Token) ahuyba786dabd86a5vdba865dvad786and
+@auth_router.post("/login")
+async def login(login_schema: LoginSchema, session: Session = Depends(get_db_session)):
+    '''Endpoint to login a user and return a JWT token'''
+    user = session.query(User).filter(User.email == login_schema.email).first()
+    if not user or not bcrypt_context.verify(login_schema.password, user.password):
+        raise HTTPException(status_code=401, detail="Invalid email or password")
+    else:
+        access_token = generate_jwt_token(user.id)
+        return {"access_token": access_token, "token_type": "bearer", "user_id": user.id}
